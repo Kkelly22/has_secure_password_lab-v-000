3ac08a824dcbdd9ecd496420af3b1ec1cb30ae5e
Kaitlin Kelly <kk504408@gmail.com>
2018-07-15 16:02:13 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,13 +14,13 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-    if !!session[:user_id]
-      @user = User.find(session[:user_id])
-    else
-      redirect_to '/new'
-    end
-  end
+#  def show
+#    if !!session[:user_id]
+#      @user = User.find(session[:user_id])
+#    else
+#      redirect_to '/new'
+#    end
+#  end
 
   private
 
